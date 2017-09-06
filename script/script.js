89ece6bd2ee424b1a0baeb99140859b411968972
jaygagnon <jason@jaygagnon.com>
2017-09-06 16:34:25 -0400

    Added bootstrap, changed the looks of the generator, refined the script code, added validation for the input field, added jQuery.

--- script/script.js
+++ script/script.js
@@ -1,53 +1,71 @@
-maxNumber = 69;
-maxPowerball = 26;
-
-function randomNumber(maxNumber) {
-	return Math.ceil(Math.random()*maxNumber);
-}
-function randomPowerball(maxPowerball) {
-	return Math.ceil(Math.random()*maxPowerball);
-}
-
+var maxNumber = 69;
+var maxPowerball = 26;
 var tableGen;
+var tickets = [];
 
 var genButton = document.getElementById("genGridBtn");
 genButton.onclick = function (e) {
-	tableGen = "</br><table cellpadding='5' callspacing='5'><tr><th>Number1</th><th>Number 2</th><th>Number 3</th><th>Number 4</th><th>Number 5</th><th>Powerball Number</th></tr>";
-	inputNo = parseInt(document.getElementById("inputNo").value);
-	genOneTicket();
-	checkNumbers();
-	for(i=0;i<inputNo;i++){
-		addToTable();
-		genOneTicket();
-		checkNumbers();
+	grid.innerHTML = "";
+	inputNumber = parseInt(document.getElementById("inputNo").value);
+	if(inputNumber > 0){
+		errorMessage.innerHTML = "";
+		tableGen = "</br><table cellpadding='5' callspacing='5' class='table table-bordered'><tr>" +
+			"<th>Number1</th><th>Number 2</th><th>Number 3</th><th>Number 4</th><th>Number 5</th>" +
+			"<th>Powerball Number</th></tr>";
+		initializeTicketArray();
+		generateTicket(inputNumber);
+		generateTable()
+		tableGen += "</table>";
+		grid.innerHTML = tableGen;
 	}
-	tableGen += "</table>";
-	grid.innerHTML = tableGen;
-}
-function genOneTicket(){
-	genNum1();
-	genNum2();
-	genNum3();
-	genNum4();	
-	genNum5();			
-	genNumPB();
+	else{
+		errorMessage.innerHTML = "Please enter a number greater than 0";
+	}
+
 }
 
-function checkNumbers(){
-	if(number2 == number1 || number3 == number1 || number3 == number2 || number4 == number3 || number4 == number2 || number4 == number1 || number5 == number4 || number5 == number3 || number5 == number2 || number5 == number1){
-		genOneTicket();
-		checkNumbers();
+function initializeTicketArray() {
+	for(var i = 0; i < inputNumber; i++){
+		for(var j = 0; j < 6; j++){
+			tickets.push([i, 0]);
+		}
 	}
 }
 
-function genNum1(){number1 = randomNumber(maxNumber);}
-function genNum2(){number2 = randomNumber(maxNumber);}
-function genNum3(){number3 = randomNumber(maxNumber);}
-function genNum4(){number4 = randomNumber(maxNumber);}
-function genNum5(){number5 = randomNumber(maxNumber);}
-function genNumPB(){powerballNum = randomPowerball(maxPowerball);}
+function generateTicket(inputNumber){
+	for(var i = 0; i < inputNumber; i++){
+		for(var j = 0; j < 5; j++){
+			tickets[i][j] = getNumber(maxNumber);
+			checkTicket(tickets[i], i, j);
+		}
+		tickets[i][5] = getPowerball(maxPowerball);
+	}
+}
 
-function addToTable(){
-	tableGen += "<tr><td>" + number1 + "</td><td>" + number2 + "</td><td>" + number3 + "</td><td>" + number4 + "</td><td>" + number5 + "</td><td class='redBox'>" + powerballNum + "</td></tr>"
+function checkTicket(ticket, i, j) {
+	for(var k = 0; k < 5; k++){
+		if(ticket[j] == ticket[k] && j != k){
+			tickets[i][j] = getNumber(maxNumber);
+			checkTicket(tickets[i], i, j);
+		}
+	}
+}
+
+function getNumber(maxNumber) {
+	return Math.ceil(Math.random()*maxNumber);
+}
+
+function getPowerball(maxPowerball) {
+	return Math.ceil(Math.random()*maxPowerball);
+}
+
+function generateTable(){
+	for(var i = 0; i < inputNumber; i++){
+		tableGen += "<tr>";
+		for(var j = 0; j < 5; j++){
+			tableGen += "<td>" + tickets[i][j] + "</td>";
+		}
+		tableGen += "<td class='redBox'>" + tickets[i][5]+ "</td></tr>";
+	}
 
 }
